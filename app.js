8678f4de8583e3bed808272f90a81695804ca846
fergus-g <fergus.gildea@utrack.com>
2024-12-17 23:01:05 +0000

    Hooked up backend and fixed some front end designs

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const expressLayouyt = require("express-ejs-layouts");
 
+const connectDB = require("./server/config/db");
+
 const app = express();
 const PORT = 5000 || process.env.PORT;
+
+// Connect to DB
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static("public"));
 
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,86 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
 
-// Routes
-router.get("", (req, res) => {
-  const locals = {
-    title: "Console.blog",
-    description:
-      "This is my blog where I talk about my journey to becoming a software engineer",
-  };
-  res.render("index", { locals });
+router.get("", async (req, res) => {
+  try {
+    const locals = {
+      title: "Console.blog",
+      description:
+        "This is my blog where I talk about my journey to becoming a software engineer",
+    };
+
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+      currentRoute: "/",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// GET
+// Post :id
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    };
+
+    res.render("post", {
+      locals,
+      data,
+      currentRoute: `/post/${slug}`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Post
+// Post search term
+
+router.post("/search", async (req, res) => {
+  try {
+    const locals = {
+      title: "Search",
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    };
+
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialCharacters = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialCharacters, "i") } },
+        { body: { $regex: new RegExp(searchNoSpecialCharacters, "i") } },
+      ],
+    });
+    res.render("search", { data, locals });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get("/about", (req, res) => {
